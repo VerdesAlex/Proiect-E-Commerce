8681c48fcdc7e6ba486374a92c6e6032ccac004b
VerdesAlex <verdesnicolae22@stud.ase.ro>
2026-03-12 22:50:41 +0200

    factory delete

--- Frontend/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://agnlsyoztkgxgpnljlhy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbmxzeW96dGtneGdwbmxqbGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTUxMTksImV4cCI6MjA4ODU3MTExOX0.KdRE0-NjGT9w1KJnmUbETQPym4csNHdr16xlhdTM8Es'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
